14e0b92ef3202b99289ccbb5a131370a66f9636c
HITESH DHAMELIYA <hiteshdhameliya32@gmail.com>
2023-08-16 11:30:26 +0530

    Landik MVC is ready to upload. inserted resources also.

---
+++ Landik/MVC/Landik_mvc_V1.0.0/Controllers/PagesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Landik.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+        public IActionResult FAQs()
+        {
+            return View();
+        }
+    }
+}
